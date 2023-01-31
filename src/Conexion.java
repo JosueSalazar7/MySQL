5eb9c6d73953faddaef16180dd9bc64b8f0cffc3
POO <josue.salazar@epn.edu.ec>
2023-01-31 10:29:03 -0500

    completo

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Conexion {
+import javax.xml.transform.Result;
+import java.sql.*;
+
+
+public class Conexion {
+    public Conexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection Conexion = DriverManager.getConnection("jdbc:mysql://localhost/ESTUDIANTES", "root", "Pelota2002");
+            Statement s = Conexion.createStatement();
+
+            ResultSet rs = s.executeQuery("SELECT * FROM NOM_ESTUDIANTES");
+            while (rs.next()) {
+                System.out.println(rs.getString("Nom_Estudiante") + " " + rs.getString(3));
+            }
+            Conexion.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
